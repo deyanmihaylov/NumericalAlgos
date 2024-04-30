5d7a1fb32bc930eabcf392320956bb0a7aa6b09b
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2024-04-30 23:30:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 import numpy as np
 
 setup(
     name="CubicSpline",
     ext_modules=cythonize([
-        "cinterpolate.pyx", "cinterpolate.pxd",
-    ]),
+        "cinterpolate.pyx",
+        "cinterpolate.pxd",
+    ],
+    compiler_directives={'language_level' : "3"},
+    ),
     include_dirs=[np.get_include()],
 )
